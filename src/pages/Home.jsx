47ba6021ae962bfe23f504b8339ea25fea004b29
May <abeillee@bk.ru>
2024-08-30 18:22:46 +0300

    5th day

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,50 @@
-import { Header } from "../components/Header/Header"
 import { Card } from "../components/Card/Card"
-import { cardArray } from "../constants"
 import { Sidebar } from "../components/Sidebar/Sidebar"
 import { Footer } from "../components/Footer/Footer"
+import { useOutletContext } from "react-router-dom"
 
 export const Home = () => {
+const {products} = useOutletContext()
+
+
+
+
     return (
-        <>
-            <Header />
-            <main>
-                <section className="search">
-                    <div className="container">
-                        <div className="search-box">
-                            <input type="text" />
-                            <button className="btn btn-primary search-btn">
-                                <img className="search-btn__icon" src="/images/search.svg" alt="search" />
-                                <span className="search-btn__text">Найти</span>
-                            </button>
+        <section className="content">
+            <div className="container">
+                <div className="content-box">
+                    <div className="content-main">
+                        <h2 className="content-main__title">Рекомендации для вас</h2>
+
+                        <div className="content-main__list">
+                            {
+                                products.map(card => (
+                                    <Card
+                                        key={card.id}
+                                        id={card.id}
+                                        title={card.title}
+                                        price={card.price}
+                                        address={card.address}
+                                        date={card.date}
+                                        img={card.img}
+                                    />
+                                ))
+                            }
                         </div>
                     </div>
-                </section>
 
-                <section className="content">
-                    <div className="container">
-                        <div className="content-box">
-                            <div className="content-main">
-                                <h2 className="content-main__title">Рекомендации для вас</h2>
+                    <div className="content-side">
+                        <h3 className="content-side__title">Сервисы и услуги</h3>
 
-                                <div className="content-main__list">
-                                    {
-                                        cardArray.map(card => (
-                                            <Card
-                                                key={card.id}
-                                                title={card.title}
-                                                price={card.price}
-                                                address={card.address}
-                                                date={card.date}
-                                                img={card.img}
-                                            />
-                                        ))
-                                    }
-                                </div>
-                            </div>
+                        <div className="content-side__box">
+                            <Sidebar />
 
-                            <div className="content-side">
-                                <h3 className="content-side__title">Сервисы и услуги</h3>
+                            <Footer />
 
-                                <div className="content-side__box">
-                                    <Sidebar />
-                                    
-                                    <Footer />
-
-                                </div>
-                            </div>
                         </div>
                     </div>
-                </section>
-            </main>
-        </>
+                </div>
+            </div>
+        </section>
     )
 }
